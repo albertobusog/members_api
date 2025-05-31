1e2764e57e8dd607491ceb7424375960ca7e7878
buso <alberto.buso@thincode.com>
2025-05-30 18:32:59 -0600

    back up of main with first commit

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+class RegistrationsController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Succesfully crreated account"
+    else
+      render :new
+    end
+    #render plain: params[:user]
+  end
+
+  private
+
+  def user_params
+    params.required(:user).permit(:email, :password, :password_confirmation)
+  end
+
+end
