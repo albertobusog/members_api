882b963c245ec36d29973f5b52353a2f1894af15
buso <alberto.buso@thincode.com>
2025-06-23 18:58:18 -0600

    Update and Create Tyoes added to mutations types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,6 +5,8 @@ module Types
     field :sign_in, mutation: Mutations::SignIn
     field :sign_up, mutation: Mutations::SignUp
     field :create_pass, mutation: Mutations::CreatePass
+    field :update_pass, mutation: Mutations::UpdatePass
+    field :create_pass, mutation: Mutations::CreatePass
     
     field :test_field, String, null: false,
       description: "An example field added by the generator"
