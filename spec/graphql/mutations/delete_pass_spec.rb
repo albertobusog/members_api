09b70fbb2bd5c25bb6f556f7b12ddba48e9c8e4d
buso <alberto.buso@thincode.com>
2025-06-30 18:23:46 -0600

    Mutation Rspec DeletePass

---
+++ spec/graphql/mutations/delete_pass_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+
+Rspec.describe "DeletePass", type: :request do
+  let(:admin) { create(:user, role: "admin") }
+  let(:client) { create(:user, role: "client") }
+  let!(:pass) { create(:pass, name: "To Delete") }
+
+  let(mutation) do
+    <<-GQL
+      mutation($id:ID!) {
+        deletePass(input: { id: $id}){
+          success
+          errors
+        }
+      }
+    GQL
+  end
+
+  context "when admin deletes a pass " do
+    it "delete it successfully" do
+      post "/graphql",
+        params: {
+          query: mutation,
+          variables: { id: pass.id }
+        }.to_json,
+        headers: auth_headers(admin)
+
+      json = JSON.parse(response.body)
+      data = json["data"]["deletePass"]
+
+      expect(data["success"]).to eq(true)
+      expect(data["errors"]).to be_empty
+      expect(Pass.find_by(id: pass.id)).to be_nil
+    end
+  end
+
+  context "when client tries to delete" do
+    it "returns not authorized" do
+      post "/graphql",
+        params: {
+          query: mutation,
+          variables: { id: pass.id }
+        }.to_json,
+        headers: auth_headers(client)
+
+      json = JSON.parse(response.body)
+      error = json["errors"].first
+
+      expect(error["message"]).to eq("Not authorized")
+    end
+  end
+end
