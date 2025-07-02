eb6229400c65dc2e44bce3478a25eac437390a7b
buso <alberto.buso@thincode.com>
2025-07-01 18:01:21 -0600

    Rspec Passes Query as Admin test

---
+++ spec/graphql/mutations/admin_passes_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe "AdminPasses", type: :request do
+  let(:admin) { create(:user, role: "admin") }
+  let(:client1) { create(:user, role: "client") }
+  let(:client2) { create(:user, role: "client") }
+
+  let!(:pass1) { create(:pass, name: "Pass1") }
+  let!(:pass2) { create(:pass, name: "Pass2") }
+
+  let(:query) do
+    <<-GQL
+      query {
+        allPasses {
+          id
+          name
+          visits
+          expiresAt
+          user {
+            email
+          }
+        }
+      }
+    GQL
+  end
+
+  it "returns all passes for admin " do
+    post "/graphql",
+      params: { query: query }.to_json,
+      headers: auth_headers(admin)
+
+    json = JSON.parse(response.body)
+    data = json["data"]["allPasses"]
+
+    expect(data.count).to eq(2)
+    expect(data.map { |p| p["name"] }).to include("Pass1", "Pass2")
+  end
+end
