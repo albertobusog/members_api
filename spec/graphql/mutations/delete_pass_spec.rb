207b4afc15c020f5283b6595c742d044923e1fd4
buso <alberto.buso@thincode.com>
2025-07-09 18:38:09 -0600

    Rspec dlete pass added validations

--- spec/graphql/mutations/delete_pass_spec.rb
+++ spec/graphql/mutations/delete_pass_spec.rb
@@ -33,6 +33,40 @@ RSpec.describe "DeletePass", type: :request do
       expect(data["errors"]).to be_empty
       expect(Pass.find_by(id: pass.id)).to be_nil
     end
+
+    it "fails if pass has clients with pending visits" do
+      purchase = create(:purchase, pass: pass, user: client, remaining_visits: 3, purchase_date: Date.today, remaining_time: 10)
+    
+      post "/graphql",
+         params: {
+           query: mutation,
+           variables: { id: pass.id }
+         }.to_json,
+         headers: auth_headers(admin)
+
+      json = JSON.parse(response.body)
+      data = json["data"]["deletePass"]
+
+      expect(data["success"]).to be false
+      expect(data["errors"]).to include("Cannot delete pass with clients having pending visits")
+    end
+
+  it "fails if destroy fails internally" do
+    allow_any_instance_of(Pass).to receive(:destroy).and_return(false)
+
+    post "/graphql",
+         params: {
+           query: mutation,
+           variables: { id: pass.id }
+         }.to_json,
+         headers: auth_headers(admin)
+
+    json = JSON.parse(response.body)
+    data = json["data"]["deletePass"]
+
+      expect(data["success"]).to be false
+      expect(data["errors"]).to include("Could not delete pass")
+    end
   end
 
   context "when client tries to delete" do
