e6ad06f64c443dc543c03a0943cbac01c7cb58a5
buso <alberto.buso@thincode.com>
2025-07-31 19:01:26 -0600

    DB seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,69 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# delete prev records
+Visit.delete_all
+Purchase.delete_all
+Pass.delete_all
+User.delete_all
+
+# Create user
+admin = User.create!(
+  email: "admin@example.com",
+  password: "password",
+  role: :admin
+)
+
+client = User.create!(
+  email: "client@example.com",
+  password: "password",
+  role: :client
+)
+
+client_no_purchases = User.create!(
+  email: "client_nopurchases@example.com",
+  password: "password",
+  role: :client
+)
+
+# Create pass
+pass1 = Pass.create!(
+  name: "Yoga Basic",
+  visits: 5,
+  expires_at: Date.today + 30,
+  price: 100.00,
+  user_id: admin.id
+)
+
+pass2 = Pass.create!(
+  name: "Crossfit Pro",
+  visits: 8,
+  expires_at: Date.today + 45,
+  price: 200.00,
+  user_id: admin.id
+)
+
+# Create purchase (acc pass)
+purchase1 = Purchase.create!(
+  user_id: client.id,
+  pass_id: pass1.id,
+  remaining_visits: 5,
+  purchase_date: Time.current,
+  price: pass1.price,
+  valid_until: Date.today + 30
+)
+
+# Create visits
+3.times do
+  Visit.create!(
+    purchase_id: purchase1.id,
+    attended: true
+  )
+end
+
+2.times do
+  Visit.create!(
+    purchase_id: purchase1.id,
+    attended: false
+  )
+end
