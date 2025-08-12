25e5e9bfb7d75f5827fd43b48d5b61688a16a841
buso <alberto.buso@thincode.com>
2025-08-11 18:13:22 -0600

    Balnce method added

--- ejercicios.rb
+++ ejercicios.rb
@@ -1,6 +1,6 @@
 def contar_vocales(texto)
   s = texto.to_s.downcase
-  conteo = { "a"=>0, "e"=>0, "i"=>0, "o"=>0, "u"=>0 }
+  conteo = { "a"=>0, "e"=>0, "i"=>0, "o"=>0, "u"=>0}
   s.each_char { |ch| conteo[ch] += 1 if conteo.key?(ch) }
   conteo
 end
@@ -12,7 +12,7 @@ def balanceado?(s)
       var =+1
     elsif ch == ")"
       return false if var == 0
-      var -=1
+      var -=1  
     end
   end
   var == 0
